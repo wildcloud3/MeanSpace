edff6ff3da77a2897aead854cde52efbc36dc6ed
lizhuliang <lizhuliang@quancheng-ec.com>
2022-04-02 16:45:37 +0800

    update press content structure

--- .vuepress/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  title: "mean space",
-  description: 'some test on singular blog',
-  base: '/MeanSpace/'
-}
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "mean Space",
+  description: '平均数空间，记录一些日常、非日常的思考',
+  base: '/',
+  theme: 'vt',
+  host: 'mean.space',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/'},
+      { text: 'Math', link: '/math/'},
+      { text: 'Science', link: '/science/'},
+      { text: 'Reading', link: '/reading/'},
+      { text: 'Coding', link: '/coding/'},
+      { text: 'Stuff', link: '/stuff/'},
+      { text: 'About', link: '/about/'},
+    ]
+  }
+}
